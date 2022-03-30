191c68539e876eaf4104a6ebadcb775537fda4e0
VENKATSAIDASARI <98133671+VENKATSAIDASARI@users.noreply.github.com>
2022-03-30 01:47:37 -0500

    computation done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var mydataRouter = require('./routes/computation');
+var computationRouter = require('./routes/computation');
 var app = express();
 
 // view engine setup
